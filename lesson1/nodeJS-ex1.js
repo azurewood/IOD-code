c278c3174488ab83e3b707af0f25c0e77af902bc
Jian <wood.azure@gmail.com>
2023-02-18 12:54:11 +1300

    NodeJS excercise 1

---
+++ lesson1/nodeJS-ex1.js
@@ -0,0 +1,25 @@
+function sum(a, b){
+    return a+b
+}
+
+function minus(a,b){
+    return a -b
+}
+
+function divide(a, b){
+    return a/b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function sayHello(name){
+    console.log("Hello "+name)
+}
+
+console.log(sum(10,4))
+console.log(minus(10,4))
+console.log(divide(10,4))
+console.log(multiply(10,4))
+sayHello("Jian")
