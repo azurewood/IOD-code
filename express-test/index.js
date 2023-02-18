9183c03bec01a1e0ecc887541651195563f9983e
Jian <wood.azure@gmail.com>
2023-02-18 15:25:49 +1300

    array example

--- express-test/index.js
+++ express-test/index.js
@@ -2,3 +2,15 @@ var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 var result = sentiment.analyze('Cats are clever.');
 console.dir(result);    // Score: -2, Comparatise
+
+
+let shoppingList=["apple","kiwi fruit","banana"]
+console.log(shoppingList)
+shoppingList.unshift("pear")
+console.log(shoppingList)
+shoppingList[2]="pineapple"
+console.log(shoppingList)
+shoppingList.push("eggs")
+console.log(shoppingList)
+shoppingList.shift()
+console.log(shoppingList)
