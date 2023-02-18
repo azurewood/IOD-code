4093607633a675ff2d6c05fb542654a01201521b
Jian <wood.azure@gmail.com>
2023-02-18 16:35:48 +1300

    test first approach

---
+++ express-test/test-first.js
@@ -0,0 +1,11 @@
+function sum(a,b){
+    return a+b
+}
+
+if(sum(1,3)!=4){
+    throw new Error("sum of 1 and 3 should be 4")
+}
+
+if(sum(4,2)!=6){
+    throw new Error("sum of 4 and 2 should be 6")
+}
